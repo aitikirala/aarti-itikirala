1b65298e561eca20e687b6dbf8f9824362240a55
Aarti Itikirala <aitikira@terpmail.umd.edu>
2025-08-21 01:29:11 -0400

    some subtle changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,13 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-full">
       <div className="text-center px-4">
-        <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter animate-fade-in">Aarti Itikirala</h1>
+        <h1
+  className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter animate-fade-in
+             text-purple-600 dark:text-white"
+>
+  Aarti Itikirala
+</h1>
+
       </div>
     </div>
   )
